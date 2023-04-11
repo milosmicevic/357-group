f46558b40f63d0a346c7448f8cbf032e450c6811
Milos Micevic <milos.micevic@asioso.com>
2023-04-11 19:24:46 +0200

    Services Page

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,9 +6,9 @@ import Logo from "../../assets/logo.png";
 const Footer = () => {
   return (
     <footer className="py-10">
-      <div className="container flex justify-between items-center">
+      <div className="container flex items-center justify-between">
         <img className="w-[200px] grayscale" src={Logo} alt="logo" />
-        <p className="text-[#929292]">357 Group © 2023. All rights reserved.</p>
+        <p className="text-[#929292] m-0">357 Group © 2023. All rights reserved.</p>
       </div>
     </footer>
   );
